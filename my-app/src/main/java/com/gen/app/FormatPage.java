5f19b1c1125553f5e04159748fbc5d6671252b0f
Fabio <fabiomanuelsilvamarques@gmail.com>
2021-03-25 10:54:34 +0100

    first dev commit

---
+++ my-app/src/main/java/com/gen/app/FormatPage.java
@@ -0,0 +1,15 @@
+package com.gen.app;
+
+public class FormatPage {
+    String markdownToHtml(String markdown) {
+        return "";
+    }
+
+    void saveMetada(String metadata) {
+
+    }
+
+    void formatPage(String page) {
+
+    }
+}
---
+++ my-app/src/test/java/com/gen/app/FormatPageTest.java
@@ -0,0 +1,15 @@
+package com.gen.app;
+
+import junit.framework.TestCase;
+
+public class FormatPageTest extends TestCase {
+
+    public void testMarkdownToHtml() {
+    }
+
+    public void testSaveMetada() {
+    }
+
+    public void testFormatPage() {
+    }
+}
