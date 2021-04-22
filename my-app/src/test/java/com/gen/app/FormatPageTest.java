3cbcb59a283e18b4af4faba1e2f9973735815ad7
fabio10000 <fabiomarques10000@gmail.com>
2021-04-22 12:33:24 +0200

    bug fix related to the change of metaDataToJson method signature

--- my-app/src/test/java/com/gen/app/FormatPageTest.java
+++ my-app/src/test/java/com/gen/app/FormatPageTest.java
@@ -17,17 +17,20 @@ public class FormatPageTest{
 
     @Test
     public void testSaveMetada() {
-        String inputString = "titre: Mon premier Article\nauteur: Bertil Chapuis\ndate: 2021-03-10";
-        JsonObject outputString;
+        String inputString = "titre: Mon premier article\n" +
+                "auteur: Bertil Chapuis\n" +
+                "date: 2021-03-10\n";
+        String expectedOutput = "{\n" +
+                "\ttitre: \"Mon premier article\",\n" +
+                "\tauteur: \"Bertil Chapuis\",\n" +
+                "\tdate: \"2021-03-10\"\n" +
+                "}";
+        String outputString;
 
         FormatPage format = new FormatPage();
 
         outputString = format.metaDataToJson(inputString);
-
-        assertTrue(outputString.isJsonObject());
-        assertTrue(outputString.get("titre").getAsString().equals("Mon premier Article"));
-        assertTrue(outputString.get("auteur").getAsString().equals("Bertil Chapuis"));
-        assertTrue(outputString.get("date").getAsString().equals("2021-03-10"));
+        assertEquals(expectedOutput, outputString);
     }
 
     @Test
