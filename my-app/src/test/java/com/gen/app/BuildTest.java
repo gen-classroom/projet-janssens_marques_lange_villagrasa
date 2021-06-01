cb00e841824c13d6dac71093b7c796987f5da93f
Diego <diegovillagrasa@gmail.com>
2021-06-01 11:53:18 +0200

    Added Build watch test

--- my-app/src/test/java/com/gen/app/BuildTest.java
+++ my-app/src/test/java/com/gen/app/BuildTest.java
@@ -4,10 +4,12 @@ import com.gen.app.Build;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
@@ -47,6 +49,23 @@ public class BuildTest{
                     "</body>\n" +
                     "</html>";
 
+    String newPageContent =
+            "<html lang=\"en\">\n" +
+                    "<head>\n" +
+                    "    <meta charset=\"UTF-8\">\n" +
+                    "    <title>Mon site | Mon Premier Site</title>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                    "    <ul>\n" +
+                    "    <li><a href=\"/index.html\">home</a></li>\n" +
+                    "    <li><a href=\"/content/page.html\">page</a></li>\n" +
+                    "</ul>\n" +
+                    "    <h1>Mon titre</h1>\n" +
+                    "<h2>Mon sous-titre</h2>\n" +
+                    "<p>Ceci est un test</p>\n\n"+
+                    "</body>\n" +
+                    "</html>";
+
     @Test
     public void canBuild(){
         initSite();
@@ -62,6 +81,39 @@ public class BuildTest{
         }
         catch (IOException e) {
             System.err.println("Could not create build folder");
+            e.printStackTrace();
+            assert false;
+        }
+
+    }
+
+    @Test
+    public void canWatch(){
+        initSite();
+        Build builder = new Build();
+        builder.sitePath = sitePath;
+        builder.buildWebsite();
+        builder.watchWebsite();
+        try {
+            Path path = Paths.get(sitePath + "/content/page.md");
+            Charset charset = StandardCharsets.UTF_8;
+
+            String content = Files.readString(path, charset);
+            content = content.replaceAll("Le contenu de mon article.", "Ceci est un test");
+            Files.write(path, content.getBytes(charset));
+
+            TimeUnit.SECONDS.sleep(1);
+            String page = new String(Files.readAllBytes(Paths.get( sitePath + "/build/content/page.html")));
+
+            assertEquals(newPageContent, page);
+        }
+        catch (IOException e) {
+            System.err.println("Could not create build folder");
+            e.printStackTrace();
+            assert false;
+        }
+        catch (InterruptedException e) {
+            System.err.println("Interrupted");
             e.printStackTrace();
             assert false;
         }
